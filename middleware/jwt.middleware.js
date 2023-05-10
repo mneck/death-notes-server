5e23cdd7a7b45709c8561dd8d6fae8596ac77986
Matt N <mneckelmann@gmail.com>
2023-05-10 17:36:55 +0200

    adds jwt middleware

---
+++ middleware/jwt.middleware.js
@@ -0,0 +1,29 @@
+const { expressjwt: jwt } = require("express-jwt");
+
+// Instantiate the JWT token validation middleware
+const isAuthenticated = jwt({
+  secret: process.env.TOKEN_SECRET,
+  algorithms: ["HS256"],
+  requestProperty: "payload",
+  getToken: getTokenFromHeaders,
+});
+
+// Extracts the JWT token from the request's 'Authorization' Headers
+function getTokenFromHeaders(req) {
+  // Checks if the token is available on the request Headers
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.split(" ")[0] === "Bearer"
+  ) {
+    // Get the encoded token string and return it
+    const token = req.headers.authorization.split(" ")[1];
+    return token;
+  }
+
+  return null;
+}
+
+// Exports the middleware so that we can use it to create protected routes
+module.exports = {
+  isAuthenticated,
+};
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -9,66 +9,136 @@ const saltRounds = 10;
 // POST  /auth/signup
 router.post("/signup", (req, res, next) => {
   const { email, password } = req.body;
+
+  if (email === "" || password === "") {
+    res
+      .status(400)
+      .json({ message: "Provide email and password" });
+    return;
+  }
+
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/;
+  if (!emailRegex.test(email)) {
+    res
+      .status(400)
+      .json({ message: "Provide a valid email address." });
+    return;
+  }
+
+  const passwordRegex =
+    /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
+  if (!passwordRegex.test(password)) {
+    res.status(400).json({
+      message:
+        "Password must have at least 6 characters and contain at least one number, one lowercase letter, and one uppercase letter.",
+    });
+    return;
+  }
+
+  // Checks the users collection in case a user with the same email already exists
+  User.findOne({ email })
+    .then((foundUser) => {
+      // If the user with the same email already exists, send an error response
+      if (foundUser) {
+        res
+          .status(400)
+          .json({ message: "User already exists." });
+        return;
+      }
+
+      // If the email is unique, proceed to hash the password
+      const salt = bcrypt.genSaltSync(saltRounds);
+      const hashedPassword = bcrypt.hashSync(
+        password,
+        salt
+      );
+
+      // Create a new user in the database
+      return User.create({
+        email,
+        password: hashedPassword,
+      });
+    })
+    .then((createdUser) => {
+      // Deconstruct the newly created user object to omit the password
+      const { email, _id } = createdUser;
+
+      // Create a new object that doesn't expose the password
+      const user = { email, _id };
+
+      // Send a json response containing the user object
+      res.status(201).json({ user: user });
+    })
+    .catch((err) => {
+      console.log(err);
+      res
+        .status(500)
+        .json({ message: "Internal Server Error" });
+    });
 });
 
-// Check if email or password is an empty string
-if (email === "" || password === "") {
-  res
-    .status(400)
-    .json({ message: "Provide email and password" });
-  return;
-}
-
-// Validate email format with regex
-const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/;
-if (!emailRegex.test(email)) {
-  res
-    .status(400)
-    .json({ message: "Provide a valid email address." });
-  return;
-}
-
-// Validate password format with regex
-const passwordRegex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
-if (!passwordRegex.test(password)) {
-  res.status(400).json({
-    message:
-      "Password must have at least 6 characters and contain at least one number, one lowercase letter, and one uppercase letter.",
-  });
-  return;
-}
-
-// Check the users collection if a user with the same email already exists
-User.findOne({ email })
-  .then((foundUser) => {
-    // If the user with the same email already exists, send an error response
-    if (foundUser) {
-      res
-        .status(400)
-        .json({ message: "User already exists." });
-      return;
-    }
-
-    // If the email is unique, proceed to hash the password
-    const salt = bcrypt.genSaltSync(saltRounds);
-    const hashedPassword = bcrypt.hashSync(password, salt);
-
-    // Create a new user in the database
-    return User.create({ email, password });
-  })
-  .then((createdUser) => {
-    const { email, _id } = createdUser;
-    const user = { email, _id };
-    res.status(201).json({ user: user });
-  })
-  .catch((err) => {
-    console.log(err);
-    res
-      .status(500)
-      .json({ message: "Internal Server Error" });
-  });
-
 // POST  /auth/login
+
+// POST  /auth/login - Verifies email and password and returns a JWT
+router.post("/login", (req, res, next) => {
+  const { email, password } = req.body;
+
+  // Check if email or password are provided as empty string
+  if (email === "" || password === "") {
+    res
+      .status(400)
+      .json({ message: "Provide email and password." });
+    return;
+  }
+
+  // Check the users collection if a user with the same email exists
+  User.findOne({ email })
+    .then((foundUser) => {
+      if (!foundUser) {
+        // If the user is not found, send an error response
+        res
+          .status(401)
+          .json({ message: "User not found." });
+        return;
+      }
+
+      // Compare the provided password with the one saved in the database
+      const passwordCorrect = bcrypt.compareSync(
+        password,
+        foundUser.password
+      );
+
+      if (passwordCorrect) {
+        // Deconstruct the user object to omit the password
+        const { _id, email } = foundUser;
+
+        // Create an object that will be set as the token payload
+        const payload = { _id, email };
+
+        // Create and sign the token
+        const authToken = jwt.sign(
+          payload,
+          process.env.TOKEN_SECRET,
+          { algorithm: "HS256", expiresIn: "6h" }
+        );
+
+        // Send the token as the response
+        res.status(200).json({ authToken: authToken });
+      } else {
+        res
+          .status(401)
+          .json({
+            message: "Unable to authenticate the user",
+          });
+      }
+    })
+    .catch((err) =>
+      res
+        .status(500)
+        .json({ message: "Internal Server Error" })
+    );
+});
+
 // ...
 
 // GET  /auth/verify
